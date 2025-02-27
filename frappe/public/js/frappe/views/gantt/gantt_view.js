c6c326ed3cf6ff7119075c069e9e34a14ecb69f2
AnggardaBayu <bayuanggarda@gmail.com>
2025-02-27 14:37:31 +0700

    check : gantt view

--- frappe/public/js/frappe/views/gantt/gantt_view.js
+++ frappe/public/js/frappe/views/gantt/gantt_view.js
@@ -58,6 +58,8 @@ frappe.views.GanttView = class GanttView extends frappe.views.ListView {
 
 			// **Perbaiki dependensi agar hanya mengambil child langsung**
 			var dependencies = me.getAllDependencies(item.name, me.data).join(",") || "";
+
+			console.log(dependencies);
 	
 			var r = {
 				start: item[field_map.start],
@@ -181,6 +183,7 @@ frappe.views.GanttView = class GanttView extends frappe.views.ListView {
 		if (!this.gantt || !this.gantt.dates) return;
 	
 		holidays.forEach((holiday) => {
+			console.log(holiday);
 			let dateIndex = this.gantt.dates.findIndex(date => moment(date).format("YYYY-MM-DD") === holiday);
 		
 			if (dateIndex !== -1) {
