2fdd9ee7712b1ef1c8ebff2bc04f2e9d0c6669f9
AnggardaBayu <bayuanggarda@gmail.com>
2025-02-28 08:09:04 +0700

    fix : permission holiday

--- frappe/api.py
+++ frappe/api.py
@@ -15,11 +15,8 @@ from frappe.utils.response import build_response
 
 @frappe.whitelist()
 def get_holiday_list(parent_name):
-    import frappe
-    import json
-
-    if not frappe.has_permission("Holiday", "read"):
-        frappe.throw("You do not have permission to view Holidays", frappe.PermissionError)
+    # if not frappe.has_permission("Holiday List", "read"):
+    #     frappe.throw("You do not have permission to view Holidays", frappe.PermissionError)
 
     # Parsing input JSON agar menjadi list
     parent_name = frappe.parse_json(parent_name) if isinstance(parent_name, str) else parent_name
